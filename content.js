53d51259ecabb4a7b6addfdc937ec1b82a6effbd
lnardon <lucaslnardon@gmail.com>
2023-10-13 00:34:56 -0300

    Update 3d theme

--- content.js
+++ content.js
@@ -4,7 +4,7 @@
     };
 
     // TODO: Delete this and use only local images
-    const imageMapCloud = {        
+    const 3dMap = {        
 	js : "https://github.com/lnardon/HubIcons/blob/master/icons/3d/javascript.png?raw=true",
         ts : "https://github.com/lnardon/HubIcons/blob/master/icons/3d/typescript.png?raw=true",
         html: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/html.png?raw=true",   
@@ -14,7 +14,21 @@
         swift: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/swift.png?raw=true", 
         vue:   "https://github.com/lnardon/HubIcons/blob/master/icons/3d/vuejs.png?raw=true",
         java:  "https://github.com/lnardon/HubIcons/blob/master/icons/3d/kotlin.png?raw=true",
-        rs: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/rust.png?raw=true"
+        rs: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/rust.png?raw=true",
+	folder: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/folder_blue.png?raw=true",
+	md: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/clipboard.png?raw=true",
+	json: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/code.png?raw=true",
+	txt: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/file.png?raw=true",
+	mp4: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/play.png?raw=true",
+	mov: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/play.png?raw=true",
+	py: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/python.png?raw=true",
+	scala: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/scala.png?raw=true",
+	sql: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/sql.png?raw=true",
+	svg: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/svg.png?raw=true",
+	png: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/image.png?raw=true",
+	jpg: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/image.png?raw=true",
+	gif: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/image.png?raw=true",
+	jpeg: "https://github.com/lnardon/HubIcons/blob/master/icons/3d/image.png?raw=true"
     }
 
     const basicMap = {
@@ -36,22 +50,43 @@
 	md: "https://github.com/lnardon/HubIcons/blob/master/icons/basic/readme.svg?raw=true",
 	jsx: "https://github.com/lnardon/HubIcons/blob/master/icons/basic/react.svg?raw=true",
 	wa: "https://github.com/lnardon/HubIcons/blob/master/icons/basic/webassembly.svg?raw=true",
-	tf: "https://github.com/lnardon/HubIcons/blob/master/icons/basic/terraform.svg?raw=true" 
+	tf: "https://github.com/lnardon/HubIcons/blob/master/icons/basic/terraform.svg?raw=true"
     }
-    const files = [...[...document.querySelectorAll(".Details")][1].children[0].children].slice(1);
 
-    for(let i = 0; i < files.length;i++){
-	let parentElement = files[i].children[1];
-        let fileExtension = [...parentElement.children][0].innerText.split(".").pop();
-	let imageElement = document.createElement("img");
-	imageElement.style.width = "1rem";
-	imageElement.style.height = "1rem";
-	// imageElement.src = chrome.runtime.getURL(`/icons/${extensionToFileMap[fileExtension]}.png`);
-	imageElement.src = basicMap[fileExtension] || "https://github.com/lnardon/HubIcons/blob/master/icons/typescript.png?raw=true";
-	files[i].children[0].removeChild(files[i].children[0].children.item(0));
-	files[i].children[0].append(imageElement);
+    let files;
+    let aux = document.querySelectorAll(".Details")
+    if(aux.length > 0){
+        files = [...[...document.querySelectorAll(".Details")][1].children[0].children].slice(1);
+	for(let i = 0; i < files.length;i++){
+            let parentElement = files[i].children[1];
+            let fileExtension = [...parentElement.children][0].innerText.split(".").pop();
+            let imageElement = document.createElement("img");
+            imageElement.style.width = "1rem";
+            imageElement.style.height = "1rem";
+        	// imageElement.src = chrome.runtime.getURL(`/icons/${extensionToFileMap[fileExtension]}.png`);
+            imageElement.src = 3dMap[fileExtension] || "https://github.com/lnardon/HubIcons/blob/master/icons/typescript.png?raw=true";
+            files[i].children[0].removeChild(files[i].children[0].children.item(0));
+            files[i].children[0].append(imageElement);
+            files[i].children[0].style.display = "flex";
+            files[i].children[0].style.alignItems = "center";
+            files[i].children[0].style.justifyContent = "center";
+    	}
+    } else {
+        files = [...[...document.querySelectorAll("tbody")][0].children];
+         console.log([...files[1].children[1].children[0].children])
+	for(let i = 0; i < files.length;i++){
+            let parentElement = files[i]?.children[1]?.children[0]
+            let fileExtension = [...parentElement.children][1].innerText.split(".").pop();
+            let imageElement = document.createElement("img");
+            imageElement.style.width = "1rem";
+            imageElement.style.height = "1rem";
+            // imageElement.src = chrome.runtime.getURL(`/icons/${extensionToFileMap[fileExtension]}.png`);
+            imageElement.src = 3dMap[fileExtension] || "https://github.com/lnardon/HubIcons/blob/master/icons/typescript.png?raw=true";
+            files[i].children[0].removeChild(files[i].children[0].children.item(0));
+            files[i].children[0].append(imageElement);
 
-	files[i].children[0].style.display = "flex";
-	files[i].children[0].style.alignItems = "center";
-	files[i].children[0].style.justifyContent = "center";
+            files[i].children[0].style.display = "flex";
+            files[i].children[0].style.alignItems = "center";
+            files[i].children[0].style.justifyContent = "center";
+        }
     }
